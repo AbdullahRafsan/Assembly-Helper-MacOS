9c19c55277d42b6551e9a2fcd88e7caebc3bb3eb
Abdullah Rafsan <rafsanabdullah88@gmail.com>
2022-07-26 13:18:47 +0600

    Initial Commit

---
+++ Assembly Helper/Assembly_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assembly_HelperApp.swift
+//  Assembly Helper
+//
+//  Created by Abdullah Rafsan on 26/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Assembly_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assembly Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assembly Helper
+//
+//  Created by Abdullah Rafsan on 26/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
